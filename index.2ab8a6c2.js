6255b705be7fcfe2f7bd4f07f458f93356751b08
AlinaStadnyk <135226486+AlinaStadnyk@users.noreply.github.com>
2023-10-24 13:17:55 +0000

    Deploying to gh-pages from @ AlinaStadnyk/goit-js-hw-10@c1c8399b2adecf6bef99cea3683aede441a4810e 🚀

--- index.2ab8a6c2.js
+++
@@ -1,2 +0,0 @@
-!function(){document.querySelector("body");console.log(bodygi)}();
-//# sourceMappingURL=index.2ab8a6c2.js.map
--- index.4a8c4af2.js
+++
@@ -1,2 +0,0 @@
-document.querySelector("body");console.log(bodygi);
-//# sourceMappingURL=index.4a8c4af2.js.map
